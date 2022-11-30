6bb219a0d4db5e5f3780100871200dfc907b1506
Mateusz Kidawa <mateusz.kidawa@gmail.com>
2022-11-30 23:59:33 +0100

    zadanie domowe

--- src/models/Playlist.model.ts
+++ src/models/Playlist.model.ts
@@ -1,3 +1,5 @@
+import { model, models, Schema } from "mongoose";
+
 export interface PlaylistModel {
   color?: string;
   name: string;
@@ -8,3 +10,15 @@ export interface PlaylistModel {
 }
 
 export type PlaylistModelWithId = PlaylistModel & { id: string };
+
+const schema = new Schema<PlaylistModel>({
+  color: { type: String, default: false },
+  name: { type: String, required: true },
+  owner: { type: String, required: true },
+  slug: { type: String, required: true },
+  spotifyId: { type: String, required: true },
+  upvote: { type: Number, required: true },
+});
+
+const Playlist = models.Playlist || model<PlaylistModel>("Playlist", schema);
+export default Playlist; 
--- src/pages/api/playlist/[id].ts
+++ src/pages/api/playlist/[id].ts
@@ -1,12 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { dbConnect } from "~/libraries/mongoose.library";
+import { Playlist } from "~/models/Playlist.model";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
+  const { id } = req.query;
   await dbConnect();
-  res.status(200).send({});
+
+  if (req.method === "GET") {
+    const students = await getPlaylist(id as string);
+    res.status(200).send({ data: students });
+  } else if (req.method === "DELETE") {
+    await deletePlaylist(id as string);
+    res.status(200).send({});
+  }
+}
+
+async function getPlaylist(id: string) {
+  const result = await Playlist.findById(id);
+  if (!result) return null;
+  const student = result.toObject();
+  return {
+    name: student.name,
+  };
+}
+
+async function deletePlaylist(id: string) {
+  await Playlist.findByIdAndDelete(id);
 }
 
 export type Response = any;
